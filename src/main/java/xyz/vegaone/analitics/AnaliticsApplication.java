fde54356c7cfb85cb675a808b210f5d1d168b3f8
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-06-01 16:20:31 +0300

    Initial commit

---
+++ src/main/java/xyz/vegaone/analitics/AnaliticsApplication.java
@@ -0,0 +1,12 @@
+package xyz.vegaone.analitics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliticsApplication.class, args);
+	}
+}
---
+++ src/test/java/xyz/vegaone/analitics/AnaliticsApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.vegaone.analitics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnaliticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
